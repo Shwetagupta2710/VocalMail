519e6c96ed382566c3ede13ecffca033eb6cc5f2
Shwetagupta2710 <shwetaguptas2710@gmail.com>
2025-03-16 04:08:45 +0530

    add

--- homepage/models.py
+++ homepage/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 class UserDetails(models.Model):
+    id = models.BigAutoField(primary_key=True)
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=50)
 
 class option(models.Model):
+    id = models.BigAutoField(primary_key=True)
     option = models.CharField(max_length=100)
 
 class Details:
--- mysite/settings.py
+++ mysite/settings.py
@@ -126,3 +126,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'assets'),
 )
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
